964b50aef8558e1d3abed5e5e8bd11ac17114d67
lorakkrumova1 <lorakkrumova@abv.bg>
2016-08-23 14:59:09 +0300

    Evaluations

---
+++ OldNewTeamProject/Models/Evaluation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OldNewTeamProject.Models
+{
+    public class Evaluation
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public string Value { get; set; }
+
+        [Required]
+        public string Language { get; set; }
+
+        public int AuthorId { get; set; }
+
+        [Required]
+        public int LanguageId { get; set; }
+    }
+}
