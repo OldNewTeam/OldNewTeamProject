2aa555412ac2e022d5eca88a567c15ec0322d106
lorakkrumova1 <lorakkrumova@abv.bg>
2016-08-23 14:02:07 +0300

    Основа на проекта

---
+++ OldNewTeamProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OldNewTeamProject.Startup))]
+namespace OldNewTeamProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
