1de6e241253339b67877cf37d7770f2caf7b4595
lorakkrumova1 <lorakkrumova@abv.bg>
2016-08-26 17:14:07 +0300

    Added all home views

--- OldNewTeamProject/Controllers/HomeController.cs
+++ OldNewTeamProject/Controllers/HomeController.cs
@@ -101,5 +101,98 @@ namespace OldNewTeamProject.Controllers
             return View(languages.ToList());
         }
 
+        // GET: Ratios      
+        public ActionResult Positives()
+        {
+            var languages = db.Languages.ToList();
+            var evaluations = db.Evaluations.ToList();
+
+            foreach (var language in languages)
+            {
+                List<Evaluation> posts = new List<Evaluation>();
+
+
+                foreach (var post in evaluations)
+                {
+                    if (post.LanguageId == language.Id)
+                    {
+                        posts.Add(post);
+                    }
+                }
+                language.Posts = posts;
+                int positiveEvaluations = 0;
+                int negativeEvaluations = 0;
+                foreach (var post in language.Posts)
+                {
+                    if (post.Value == "rocks")
+                    {
+                        positiveEvaluations++;
+                    }
+                    else
+                    {
+                        negativeEvaluations++;
+                    }
+                }
+                language.positives = positiveEvaluations;
+                language.negatives = negativeEvaluations;
+                if (language.Posts.Count > 0)
+                {
+                    language.ratio = (double)positiveEvaluations / language.Posts.Count * 100;
+                }
+            }
+
+            languages = languages.OrderByDescending(l => l.positives).ToList();
+
+
+            return View(languages.ToList());
+        }
+
+
+
+        // GET: Negatives     
+        public ActionResult Negatives()
+        {
+            var languages = db.Languages.ToList();
+            var evaluations = db.Evaluations.ToList();
+
+            foreach (var language in languages)
+            {
+                List<Evaluation> posts = new List<Evaluation>();
+
+
+                foreach (var post in evaluations)
+                {
+                    if (post.LanguageId == language.Id)
+                    {
+                        posts.Add(post);
+                    }
+                }
+                language.Posts = posts;
+                int positiveEvaluations = 0;
+                int negativeEvaluations = 0;
+                foreach (var post in language.Posts)
+                {
+                    if (post.Value == "rocks")
+                    {
+                        positiveEvaluations++;
+                    }
+                    else
+                    {
+                        negativeEvaluations++;
+                    }
+                }
+                language.positives = positiveEvaluations;
+                language.negatives = negativeEvaluations;
+                if (language.Posts.Count > 0)
+                {
+                    language.ratio = (double)positiveEvaluations / language.Posts.Count * 100;
+                }
+            }
+
+            languages = languages.OrderByDescending(l => l.negatives).ToList();
+
+
+            return View(languages.ToList());
+        }
     }
 }
